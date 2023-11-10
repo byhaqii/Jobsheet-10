aee4c97e5f916816eda76b51f0b49a22120418ab
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-11-10 14:15:25 +0700

    Question 4

--- Cinema19.java
+++ Cinema19.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Cinema19 {
     public static void main(String[] args) {
-        
+
         String [][] audience = new String[4][2];
         audience[0][0] = "Amin";
         audience[0][1] = "Bena";
@@ -10,7 +10,14 @@ public class Cinema19 {
         audience[2][0] = "Eka";
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
-        
+        audience[3][1] = "Hana";
+
+        System.out.println(audience.length);
+        System.out.println(audience[0].length);
+        System.out.println(audience[1].length);
+        System.out.println(audience[2].length);
+        System.out.println(audience[3].length);
+
         System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
         System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
