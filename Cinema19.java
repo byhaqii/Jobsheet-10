7afe1bf420324c9ef55588dd80814fba5d3e2a2a
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-11-17 13:37:10 +0700

    tes

--- Cinema19.java
+++ Cinema19.java
@@ -18,9 +18,9 @@ public class Cinema19 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
-        System.out.println(audience.length);
-        for (int i = 0; i < audience.length; i++){
-            System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
+        System.out.println("Audiences in the row 3: ");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.print(audience[2][i]);
         }
     }
 }
