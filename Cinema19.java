ee32820cb30d5f5ccd8225da6f030d22800b6815
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-11 14:35:36 +0700

    question 12

--- Cinema19.java
+++ Cinema19.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-
 public class Cinema19 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String[][] audience = new String[4][2];
-        audience[0][0] = "Amin";
-        audience[0][1] = "Bena";
-        audience[1][0] = "Candra";
-        audience[1][1] = "Dela";
-        audience[2][0] = "Eka";
-        audience[2][1] = "Farhan";
-        audience[3][0] = "Gisel";
-        audience[3][1] = "Hana";
-        System.out.printf("%s \t %s \n", audience[0][0], audience[0][1]);
-        System.out.printf("%s \t %s \n", audience[1][0], audience[1][1]);
-        System.out.printf("%s \t %s \n", audience[2][0], audience[2][1]);
-        System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
-        System.out.println(audience.length);
-        System.out.println("Audience in the row 3 : ");
-        for (int i = 0; i < audience[2].length; i++) {
-            System.out.println(audience[2][i]);
-            for (String j : audience[2]) {
-                System.out.println(j);
-            }
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+String[][] audience = new String[4][2];
+audience[0][0] = "Amin";
+audience[0][1] = "Bena";
+audience[1][0] = "Candra";
+audience[1][1] = "Dela";
+audience[2][0] = "Eka";
+audience[2][1] = "Farhan";
+audience[3][0] = "Gisel";
+audience[3][1] = "Hana";
+System.out.printf("%s \t %s \n",audience[0][0],audience[0][1]);
+System.out.printf("%s \t %s \n",audience[1][0],audience[1][1]);
+System.out.printf("%s \t %s \n",audience[2][0],audience[2][1]);
+System.out.printf("%s \t %s \n",audience[3][0],audience[3][1]);
+System.out.println(audience.length);
+System.out.println("Audience in the row 3 : ");
+for(String i : audience[2]){
+System.out.println(i);
+for(int j = 0;j< audience.length;j++){
+System.out.println("Audience in the row " +(i+1)+ String.join(",", audience [j]));
+}
 
-        }
-    }
+
+}    
+}
 }
