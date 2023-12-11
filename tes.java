a81e8ed2d888939fa104a1d52a3f0b0722b36e4d
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-11 11:33:31 +0700

    2

---
+++ tes.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class tes {
+    public static void main(String[] args) {
+        int[][] prices = new int[5][3];
+        System.out.println(prices[3][2]);
+        }
+    }
