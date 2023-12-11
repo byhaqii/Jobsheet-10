bebc44a7329573d0e022b88ff0e95adc6fd945d7
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-11 13:32:34 +0700

    question10

--- Cinema19.java
+++ Cinema19.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class Cinema19 {
     public static void main(String[] args) {
-
-        String [][] audience = new String[4][2];
+        Scanner scan = new Scanner(System.in);
+        String[][] audience = new String[4][2];
         audience[0][0] = "Amin";
         audience[0][1] = "Bena";
         audience[1][0] = "Candra";
@@ -11,19 +12,19 @@ public class Cinema19 {
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
-
-        
-        System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
-        System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
-        System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
-        System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
-
-        for(int i = 0;i<audience.length;i++){
-            System.out.println("Length of row" +(i+1) + ":" + audience[i].length);
+        System.out.printf("%s \t %s \n", audience[0][0], audience[0][1]);
+        System.out.printf("%s \t %s \n", audience[1][0], audience[1][1]);
+        System.out.printf("%s \t %s \n", audience[2][0], audience[2][1]);
+        System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
+        System.out.println(audience.length);
 
         for (String[] rowAudience : audience) {
-            System.out.println("Length of row: " + rowAudience.length);
+            System.out.println("Length of row  : " + rowAudience.length);
+            System.out.println("Audience in the row 3 : ");
+            for (int i = 0; i < audience[2].length; i++) {
+                System.out.println(audience[2][i]);
+            }
+
         }
-    }
     }
 }
