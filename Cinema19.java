14f89a7a4c82c5b202f52b7745aee9f51356cdba
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-11 14:31:34 +0700

    question 11

--- Cinema19.java
+++ Cinema19.java
@@ -17,12 +17,11 @@ public class Cinema19 {
         System.out.printf("%s \t %s \n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
         System.out.println(audience.length);
-
-        for (String[] rowAudience : audience) {
-            System.out.println("Length of row  : " + rowAudience.length);
-            System.out.println("Audience in the row 3 : ");
-            for (int i = 0; i < audience[2].length; i++) {
-                System.out.println(audience[2][i]);
+        System.out.println("Audience in the row 3 : ");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.println(audience[2][i]);
+            for (String j : audience[2]) {
+                System.out.println(j);
             }
 
         }
